3665069199d967c403c953788c3f48bb3bcb5b16
KonstantinSh <Kverde@yandex.ru>
2021-10-03 23:30:57 +0500

    upd

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const path = require('path');
 
 const siteBuilder = require('./src/site_builder.js');
 
-siteBuilder.build('..', path.dirname(__filename));
+siteBuilder.build(path.join(path.dirname(__filename), '..'), path.dirname(__filename));
