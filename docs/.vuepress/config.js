66b6ede8c1c3817f4ff3b78dbde30825491e1210
KonstantinSh <Kverde@yandex.ru>
2021-10-13 21:14:57 +0500

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,37 @@ const workDir = path.dirname(__filename);
 const jsonConfigPath = path.join(workDir, 'config.json');
 const json_config = JSON.parse(fs.readFileSync(jsonConfigPath, 'utf-8'));
 
+const makeRevisionmeHeader = (config) => {
+  if (!config.revisionmeProjectId) {
+    return [];
+  }
+  
+  return [
+    	['script', {}, `
+	    var __rm__config = {
+			projectId: '${config.revisionmeProjectId}',
+			locale: 'ru',
+			contextWidget: 0,
+			embedBtn: 0,
+			floatingBtn: 0,
+			floatingBtnPosition: 'left',
+			floatingBtnStyle: 'light',
+			};
+    `],
+    ['script', { src: "https://widget.revisionme.com/app.js", defer: "defer", id: "rm_app_script", }],
+  ]
+}
+
+const revisionmeHeader = makeRevisionmeHeader(json_config);
+
 module.exports = {
   lang: 'ru-RU',
   title: json_config.title,
   base: json_config.base,
   description: '',
+  head: [
+    ...revisionmeHeader
+  ],
   themeConfig: {
     contributors: false,
     lastUpdatedText: 'Последние обновление',
