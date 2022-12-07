c95577645bed3b0bca45a5502cbe0964da4004af
KonstantinSh <Kverde@yandex.ru>
2022-12-07 23:13:18 +0500

    change makrdown-it page number package

--- 4_update.bat
+++ 4_update.bat
@@ -2,7 +2,7 @@ call yarn add -D markdown-it-footnote
 call yarn add -D @vuepress/plugin-search
 call yarn add -D @vuepress/shared-utils
 
-call yarn add -D @zarya/markdown-it-book-page-number
+call yarn add -D @comtext/markdown-it-book-page-number
 call yarn add -D @traptitech/markdown-it-katex
 
 call yarn add -D vuepress
