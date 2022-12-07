afb69e69cdcb43c30a5fdf0fa15e07f3758fccef
KonstantinSh <Kverde@yandex.ru>
2022-12-07 23:56:21 +0500

    Форматирование кода

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const dotenv = require('dotenv');
+const path = require("path");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const siteBuilder = require('./src/site_builder.js');
+const siteBuilder = require("./src/site_builder.js");
 
 const rootDir = path.dirname(__filename);
-const booksDir = process.env.PATH_TO_BOOKS ?? path.join(rootDir, '..');
+const booksDir = process.env.PATH_TO_BOOKS ?? path.join(rootDir, "..");
 
 siteBuilder.build(booksDir, rootDir);
